52b25bb77d50749083069f88259d1746a861beb6
Jicheng Zhi <jzhi0004@student.monash.edu>
2024-07-02 10:26:26 +1000

    feat: persist readings from MQ after server start up

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,42 @@
+import { connectAsync } from "async-mqtt";
+import { parse } from "mathjs";
 import { app } from "./app";
 import { db } from "./database";
-import { port } from "./env";
+import { mqttBroker, port } from "./env";
+import { messageToReading } from "./mq/parser";
 
 await db.connect();
+
+const client = await connectAsync(mqttBroker, {
+	protocol: "mqtt",
+	clientId: "openaurae-server",
+});
+
+await client.subscribe("zigbee/#");
+await client.subscribe("air-quality/#");
+
+client.on("message", async (topic, messageBuffer) => {
+	const message = JSON.parse(messageBuffer.toString());
+
+	console.log(`${topic} - ${JSON.stringify(message)}`);
+
+	const reading = messageToReading(topic, message);
+
+	await db.insertReading(reading);
+
+	const corrections = await db.sensorCorrections(
+		reading.device,
+		reading.reading_type,
+	);
+
+	for (const correction of corrections) {
+		const metricName = correction.metric;
+		reading[metricName] = parse(correction.expression).evaluate(reading);
+	}
+
+	reading.processed = true;
+	await db.insertReading(reading);
+});
 
 export default {
 	port: port,
